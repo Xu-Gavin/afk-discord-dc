0705dafcd19564a3db9f5af701109ba10a8e425c
Gavin Xu <gavinxu100@gmail.com>
2025-07-05 15:50:53 -0400

    Replace Marco's icon with hang up icon

--- afk-discord-dc.py
+++ afk-discord-dc.py
@@ -22,7 +22,7 @@ class AfkMonitoringApp:
         # Setup root
         root.title("Discord AFK Auto Kicker")
         root.geometry("300x200")
-        icon = tk.PhotoImage(file="Screenshot_2025-01-03_202415.png")
+        icon = tk.PhotoImage(file="assets/icon.png")
         root.iconphoto(True, icon)
         self.title_text = tk.StringVar(value="AFK Monitoring Idle")
         label = tk.Label(root, textvariable=self.title_text)
